931c7c2e8393f6e576bf712099831d81ea83ff40
David Rivera <David.R.Rivera193@gmail.com>
2013-12-18 22:45:47 -0500

    cleaning

--- public/js/cart.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $('.fadeload').hover(function() {
-        $(this).find('img').fadeTo(500, 0.5);
-    }, function() {
-        $(this).find('img').fadeTo(500, 1);
-    });
-
-}); 
